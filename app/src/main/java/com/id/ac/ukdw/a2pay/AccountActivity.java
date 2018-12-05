db33e4fc46a2ea248d70a2d26ea712a7e537e1a6
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2018-12-05 12:48:42 +0700

    update account 2

--- app/src/main/java/com/id/ac/ukdw/a2pay/AccountActivity.java
+++ app/src/main/java/com/id/ac/ukdw/a2pay/AccountActivity.java
@@ -2,8 +2,15 @@ package com.id.ac.ukdw.a2pay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AccountActivity extends AppCompatActivity {
+    private Button btnClose;
+    private Button btnEditAcc;
+    private TextView txtUsername;
+    private TextView txtTelepon;
+    private TextView txtBalance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
